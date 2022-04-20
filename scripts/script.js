a17bfc867f3bc12fa166008249b0ec1cc76140de
Алексей Фёдоров <67751797+RLYamakasi@users.noreply.github.com>
2022-04-20 19:04:03 +0300

    4 спринт еще больше исправлено ошибок

--- scripts/script.js
+++ scripts/script.js
@@ -4,10 +4,10 @@ let popUp = main.querySelector('.pop-up');
 let popCloseButton = main.querySelector('.pop-up__close-button');
 let popEdit = main.querySelector('.profileInfo__button');
 function popClose(){
-    popUp.classList.add('pop-upHide');
+    popUp.classList.add('pop-up_hide');
 }
 function popOpen(){
-    popUp.classList.remove('pop-upHide');
+    popUp.classList.remove('pop-up_hide');
 }
 popCloseButton.addEventListener('click', popClose);
 popEdit.addEventListener('click', popOpen);
@@ -26,7 +26,7 @@ function formSubmitHandler (evt) {
                               
     name.textContent = nameInput.value;
     job.textContent = jobInput.value;
-    popUp.classList.add('pop-upHide');
+    popUp.classList.add('pop-up_hide');
 }
 
 saveButton.addEventListener('click', formSubmitHandler); 
